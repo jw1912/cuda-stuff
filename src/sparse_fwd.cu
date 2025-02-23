1e197eee19ea47b8b1944999654beb2cbd961780
jw1912 <jamie.whiting.dev@gmail.com>
2025-02-23 16:46:59 +0000

    simplify

--- src/sparse_fwd.cu
+++ src/sparse_fwd.cu
@@ -1,109 +1,6 @@
 #include "util.cu"
 
-__global__ void sparseKernel1(const size_t max_active, const size_t output_size, const float* weights, const int32_t* inputs, float* outputs)
-{
-    const size_t elem = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (elem >= output_size)
-        return;
-
-    const size_t inputIdx = max_active * blockIdx.y;
-    const int32_t* thisInput = inputs + inputIdx;
-    float* thisOutput = outputs + output_size * blockIdx.y + elem;
-
-    float ourElementVal = 0.0F;
-
-    for (size_t i = 0; i < max_active; i++) {
-        const int32_t inp = thisInput[i];
-
-        if (inp == -1)
-            break;
-
-        const size_t ourIdx = static_cast<size_t>(inp) * output_size + elem;
-        ourElementVal += weights[ourIdx];
-    }
-
-    thisOutput[0] = ourElementVal;
-}
-
-__global__ void sparseKernel2(const size_t max_active, const size_t output_size, const float* weights, const int32_t* inputs, float* outputs)
-{
-    extern __shared__ int32_t shared_input_indices[];
-    const size_t elem = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (elem >= output_size)
-        return;
-
-    if (threadIdx.x < max_active)
-    {
-        const size_t input_idx = max_active * blockIdx.y;
-        const int32_t* this_input = inputs + input_idx;
-
-        for (size_t i = threadIdx.x; i < max_active; i += blockDim.x)
-        {
-            shared_input_indices[i] = this_input[i];
-        }
-    }
-
-    __syncthreads();
-
-    float val = 0.0F;
-
-    for (size_t i = 0; i < max_active; i++) {
-        const int32_t inp = shared_input_indices[i];
-
-        if (inp == -1)
-            break;
-
-        const size_t our_idx = static_cast<size_t>(inp) * output_size + elem;
-        val += weights[our_idx];
-    }
-
-    outputs[output_size * blockIdx.y + elem] = val;
-}
-
-// this kernel only works when output_size is a multiple of 4
-__global__ void sparseKernel3(const size_t max_active, const size_t output_size, const float* weights, const int32_t* inputs, float* outputs)
-{
-    extern __shared__ int32_t shared_input_indices[];
-    const size_t elem = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (4 * elem >= output_size)
-        return;
-
-    if (threadIdx.x < max_active)
-    {
-        const size_t input_idx = max_active * blockIdx.y;
-        const int32_t* this_input = inputs + input_idx;
-
-        for (size_t i = threadIdx.x; i < max_active; i += blockDim.x)
-        {
-            shared_input_indices[i] = this_input[i];
-        }
-    }
-
-    __syncthreads();
-
-    float val[4] = {0.0F, 0.0F, 0.0F, 0.0F};
-
-    for (size_t i = 0; i < max_active; i++) {
-        const int32_t inp = shared_input_indices[i];
-
-        if (inp == -1)
-            break;
-
-        const size_t our_idx = static_cast<size_t>(inp) * output_size / 4;
-        const float4 a = ((const float4 *)weights)[our_idx + elem];
-        val[0] += a.x;
-        val[1] += a.y;
-        val[2] += a.z;
-        val[3] += a.w;
-    }
-
-    ((float4 *)outputs)[output_size * blockIdx.y / 4 + elem] = make_float4(val[0], val[1], val[2], val[3]);
-}
-
-__global__ void sparseKernel4(const size_t m, const size_t nnz, const float* A, const int32_t* x, float* y)
+__global__ void sparseKernel1(const size_t m, const size_t nnz, const float* A, const int32_t* x, float* y)
 {
     const int row = threadIdx.x + blockDim.x * blockIdx.x;
     const int32_t* tx = x + nnz * blockIdx.y;
@@ -122,6 +19,35 @@ __global__ void sparseKernel4(const size_t m, const size_t nnz, const float* A, 
         }
 
         ty[row] = sum;
+    }
+}
+
+// this kernel only works when output_size is a multiple of 4
+__global__ void sparseKernel2(const size_t m, const size_t nnz, const float* A, const int32_t* x, float* y)
+{
+    const size_t row = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (4 * row < m)
+    {
+    
+        float val[4] = {0.0F, 0.0F, 0.0F, 0.0F};
+        const int32_t* tx = x + nnz * blockIdx.y;
+    
+        for (size_t i = 0; i < nnz; i++) {
+            const int32_t inp = tx[i];
+    
+            if (inp != -1)
+            {
+                const size_t our_idx = static_cast<size_t>(inp) * m / 4;
+                const float4 a = ((const float4 *)A)[our_idx + row];
+                val[0] += a.x;
+                val[1] += a.y;
+                val[2] += a.z;
+                val[3] += a.w;
+            }
+        }
+    
+        ((float4 *)y)[m * blockIdx.y / 4 + row] = make_float4(val[0], val[1], val[2], val[3]);
     }
 }
 
@@ -168,14 +94,11 @@ int main()
     {
         const size_t numChunks = (outputs + threadsPerBlock - 1) / threadsPerBlock;
         dim3 grid(numChunks, batch_size);
-        sparseKernel1<<<grid, threadsPerBlock>>>(max_active, outputs, weights_gpu, in, out);
+        sparseKernel1<<<grid, threadsPerBlock>>>(outputs, max_active, weights_gpu, in, out);
     }
 
     float *expected = new float[outputs * batch_size];
     cudaMemcpy((void *)expected, (const void *)out, sizeof(float) * outputs * batch_size, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
-
-    check_equal(outputs * batch_size, expected, out);
     cudaMemset(out, 0, sizeof(float) * outputs * batch_size);
     cudaDeviceSynchronize();
 
@@ -183,36 +106,10 @@ int main()
 
     for (size_t i = 0; i < reps; i++)
     {
-        const size_t numChunks = (outputs + threadsPerBlock - 1) / threadsPerBlock;
-        dim3 grid(numChunks, batch_size);
-        sparseKernel2<<<grid, threadsPerBlock, max_active * sizeof(int32_t)>>>(max_active, outputs, weights_gpu, in, out);
-    }
-
-    check_equal(outputs * batch_size, expected, out);
-    cudaMemset(out, 0, sizeof(float) * outputs * batch_size);
-    cudaDeviceSynchronize();
-
-    std::cout << "Running sparseKernel3" << std::endl;
-
-    for (size_t i = 0; i < reps; i++)
-    {
         const size_t output_chunks = (outputs + 3) / 4;
         const size_t numChunks = (output_chunks + threadsPerBlock - 1) / threadsPerBlock;
         dim3 grid(numChunks, batch_size);
-        sparseKernel3<<<grid, threadsPerBlock, max_active * sizeof(int32_t) + sizeof(float) * threadsPerBlock>>>(max_active, outputs, weights_gpu, in, out);
-    }
-
-    check_equal(outputs * batch_size, expected, out);    
-    cudaMemset(out, 0, sizeof(float) * outputs * batch_size);
-    cudaDeviceSynchronize();
-
-    std::cout << "Running sparseKernel4" << std::endl;
-
-    for (size_t i = 0; i < reps; i++)
-    {
-        const size_t numChunks = (outputs + threadsPerBlock - 1) / threadsPerBlock;
-        dim3 grid(numChunks, batch_size);
-        sparseKernel4<<<grid, threadsPerBlock>>>(outputs, max_active, weights_gpu, in, out);
+        sparseKernel2<<<grid, threadsPerBlock>>>(outputs, max_active, weights_gpu, in, out);
     }
 
     check_equal(outputs * batch_size, expected, out);
