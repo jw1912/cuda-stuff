e054f76bb8d82417ff2f455f6294ed2a82376902
jw1912 <jamie.whiting.dev@gmail.com>
2025-03-09 02:11:24 +0000

    Simplify some code

--- src/select_matmul/2_ptrs_bmv.cu
+++ src/select_matmul/2_ptrs_bmv.cu
@@ -11,35 +11,6 @@ __global__ void ptrs(const int32_t size, const int32_t stride, const int32_t* s,
     }
 }
 
-void bmv(
-    cublasHandle_t handle, 
-    const int32_t batch_size,
-    const int32_t input_size,
-    const int32_t output_size,
-    const int32_t ldA,
-    float** A,
-    float** x,
-    float** y)
-{
-    const int32_t m = output_size;
-    const int32_t n = input_size;
-
-    const float alpha = 1.0F;
-    const float beta = 0.0F;
-
-    cublasSgemvBatched(
-        handle,
-        CUBLAS_OP_N,
-        m, n,
-        &alpha,
-        A, ldA,
-        x, 1,
-        &beta,
-        y, 1,
-        batch_size
-    );
-}
-
 void PtrsThenBMV(
     cublasHandle_t handle,
     const int32_t batch_size,
@@ -63,6 +34,8 @@ void PtrsThenBMV(
 
     const int32_t stride = (interleave) ? output_size : output_size * buckets;
     const int32_t ldA = (interleave) ? output_size * buckets : output_size;
+    const float alpha = 1.0F;
+    const float beta = 0.0F;
 
     const size_t threads = 512;
     const size_t blocks = (batch_size + threads - 1) / threads;
@@ -70,5 +43,16 @@ void PtrsThenBMV(
     ptrs<<<blocks, threads>>>(batch_size, stride, s, A, As);
     ptrs<<<blocks, threads>>>(batch_size, input_size, nullptr, x, xs);
     ptrs<<<blocks, threads>>>(batch_size, output_size, nullptr, y, ys);
-    bmv(handle, batch_size, input_size, output_size, ldA, As, xs, ys);
+
+    cublasSgemvBatched(
+        handle,
+        CUBLAS_OP_N,
+        output_size, input_size,
+        &alpha,
+        As, ldA,
+        xs, 1,
+        &beta,
+        ys, 1,
+        batch_size
+    );
 }
